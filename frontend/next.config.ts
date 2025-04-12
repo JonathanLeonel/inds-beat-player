36c44108e0f6792f27859a0ea80bcfdb56839f92
Industriales Prods <industriales@Industrialess-MacBook-Pro.local>
2025-04-12 08:55:59 -0300

    feat: Added basic styles

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/beats",
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
   // /* config options here */
   // async rewrites() {
   //   return [
